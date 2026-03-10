7723089d215ea734900dfb6bc4fb13683888ccde
hctmounika <mounika.gollu@humancloud.co.in>
2026-03-10 11:48:34 +0530

    Add JUnit tests

---
+++ src/test/java/com/example/TaskProcessorTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskProcessorTest {
+
+    @Test
+    void simpleTest() {
+
+        assertEquals(4, 2 + 2);
+
+    }
+}
